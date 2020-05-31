dfe958dd02a30cdd80e87f8a020c12cb0481eb5d
Harshith <harshithprakashmutta@gmail.com>
2020-05-31 23:16:18 +0300

    Update docs of Game.java and Room.java

--- src/main/java/com/hpm/textadventure/Game.java
+++ src/main/java/com/hpm/textadventure/Game.java
@@ -1,6 +1,11 @@
 package com.hpm.textadventure;
 
-// Carries information about the game, current room, etc.
+/**
+ * A class that carries info about the game along with an instance of the player
+ * TEMPORARY
+ * @author Harshith
+ * @version 1.0.0
+ */
 public class Game {
     Player player;
     Room currentRoom;
--- src/main/java/com/hpm/textadventure/Room.java
+++ src/main/java/com/hpm/textadventure/Room.java
@@ -2,6 +2,12 @@ package com.hpm.textadventure;
 
 import java.util.ArrayList;
 
+/**
+ * A class holding data about the various rooms the player will encounter
+ * TEMPORARY
+ * @author Harshith
+ * @version 1.0.0
+ */
 public class Room {
     ArrayList<String> obstacles;
 }
