4968f6f3aa18e868c634d77c36a4024870891161
Harshith <harshithprakashmutta@gmail.com>
2020-05-31 23:17:48 +0300

    Update docs of Character.java

--- src/main/java/com/hpm/textadventure/Character.java
+++ src/main/java/com/hpm/textadventure/Character.java
@@ -1,10 +1,20 @@
 package com.hpm.textadventure;
 
+/**
+ * The main character class from which the player data and the NPC data is taken
+ * @author Harshith
+ * @version 1.0.0
+ */
 public class Character {
     int hp;
     String name;
     String description;
 
+    /**
+     * A pretty simple constructor to initialize all properties of the Character
+     * @param name The name of the character
+     * @param description A description of the character
+     */
     public Character(String name, String description) {
         this.name = name;
         this.description = description;
