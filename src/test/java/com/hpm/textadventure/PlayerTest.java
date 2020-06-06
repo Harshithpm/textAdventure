c1d89778bbfebbf064004f27a7f55a691dd07fdc
Harshith <harshithprakashmutta@gmail.com>
2020-06-06 20:57:49 +0300

    Make tests for player initialisation

--- src/test/java/com/hpm/textadventure/PlayerTest.java
+++ src/test/java/com/hpm/textadventure/PlayerTest.java
@@ -1,4 +1,27 @@
 package com.hpm.textadventure;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class PlayerTest {
+    @Test
+    void testPlayerInit() {
+        Player player = new Player(Player.PlayerType.KNIGHT, "test", "test");
+        ArrayList<HashMap<String, Integer>> stats = new ArrayList<>();
+        HashMap<String, Integer> temp = new HashMap<>();
+        temp.put("Attack", 25);
+        temp.put("Defence", 20);
+        temp.put("Magic", 0);
+        stats.add(temp);
+        assertEquals("test", player.name);
+        assertEquals("test", player.description);
+        assertEquals(Player.PlayerType.KNIGHT, player.playerType);
+        assertEquals("test", player.description);
+        assertEquals(60, player.hp);
+        assertEquals(stats, player.stats);
+    }
 }
